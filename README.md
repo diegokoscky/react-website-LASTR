a2961525eb1f816a32aa048119150f784bdbb1f9
Diego Koscky <2228939+diegokoscky@users.noreply.github.com>
2021-03-24 18:29:20 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Website com o Create React App + Sass
+
+Esse website foi criado com [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá Mundo!</h1>
+      <p>Website em React.js</p>
+    </div>
+  );
+}
+
+export default App;
